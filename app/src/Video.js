2f86c8d7e00d22677234c9f1a631f723e2ca77ad
Pelle Almquist <per.almquist@gmail.com>
2017-10-21 20:54:51 +0200

    Video playback

--- app/src/Video.js
+++ app/src/Video.js
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, {Component} from 'react'
 
+import playButton from './play-button.svg'
 import './Video.css'
 
-function Video({video}) {
-    console.log({video})
+class Video extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isPlaying: false,
+    }
+  }
+
+  render() {
+    const {video} = this.props
+    const {isPlaying} = this.state
     return (
       <div className="Video">
-        <img className="Video-poster" alt="video-poster" src={video.imgUrl} />
+        <video
+          ref="video"
+          className="Video-player"
+          width="99%"
+          height="99%"
+          playsInline
+          controls={isPlaying ? 1 : 0}
+          onEnded={() => this.setState({isPlaying: false})}
+        >
+          <source src={video.videoUrl} />
+        </video>
+        {
+          !isPlaying &&
+          <img
+            className="Video-play-button"
+            alt="play-button"
+            src={playButton}
+            onClick={() => {
+              this.setState({isPlaying: true})
+              this.refs.video.play()
+            }}
+            />
+        }
       </div>
     )
+  }
 }
-
 export default Video
--- app/src/visiblePosts.js
+++ app/src/visiblePosts.js
@@ -1,7 +1,16 @@
 const videos = [
-  {imgUrl: 'https://i.ytimg.com/vi/LPjblqE3xHk/hqdefault.jpg'},
-  {imgUrl: 'https://i.ytimg.com/vi/7LjRwpLmzS4/mqdefault.jpg'},
-  {imgUrl: 'https://i.ytimg.com/vi/liSseCH6_p8/mqdefault.jpg'},
+  {
+    imgUrl: 'https://i.ytimg.com/vi/LPjblqE3xHk/hqdefault.jpg',
+    videoUrl: 'https://www.videvo.net/videvo_files/converted/2014_12/videos/Raindrops_Videvo.mp485639.mp4'
+  },
+  {
+    imgUrl: 'https://i.ytimg.com/vi/7LjRwpLmzS4/mqdefault.jpg',
+    videoUrl: 'https://www.videvo.net/videvo_files/converted/2014_12/videos/Raindrops_Videvo.mp485639.mp4'
+  },
+  {
+    imgUrl: 'https://i.ytimg.com/vi/liSseCH6_p8/mqdefault.jpg',
+    videoUrl: 'https://www.videvo.net/videvo_files/converted/2014_12/videos/Raindrops_Videvo.mp485639.mp4'
+  },
 ]
 
 export default [0, 1, 2].map(i => {
@@ -9,7 +18,7 @@ export default [0, 1, 2].map(i => {
     id: `${i}`,
     title: `Title ${i}`,
     video: videos[i],
-    votes: {up: i * 2.4, down: i * 1.1},
+    votes: {up: (i + 1) * 2.4, down: (i + 1) * 1.1},
     textBody: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   }
 })
