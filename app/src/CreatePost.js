77f59c4b1074efb8c284fb6afcf3c7d07546e27d
Pelle Almquist <per.almquist@gmail.com>
2017-10-22 17:42:39 +0200

    CSS fixes

--- app/src/CreatePost.js
+++ app/src/CreatePost.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import './CreatePost.css'
+
 function CreatePost() {
   return (
     <div className="CreatePost">
-      CreatePost
+      <h1 className="CreatePost-header">Create a new post</h1>
+      <form className="CreatePost-table">
+        <div className="CreatePost-row">
+          <label className="CreatePost-label" htmlFor="title">Title:</label>
+          <input className="CreatePost-data" id="title" type="text" required maxLength="64" autoFocus/>
+        </div>
+        <div className="CreatePost-row">
+          <label className="CreatePost-label" htmlFor="description">Description:</label>
+          <textarea className="CreatePost-data" id="description" type="text" required maxLength="256" rows="4" />
+        </div>
+        <div className="CreatePost-row">
+          <label className="CreatePost-label" htmlFor="video">Video:</label>
+          <input className="CreatePost-data" id="video" type="file" required />
+        </div>
+        <input className="CreatePost-button" type="submit" value="Create" />
+      </form>
     </div>
   )
 }
