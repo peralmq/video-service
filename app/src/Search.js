27a529c46240df55818b1053dbe41fe9a5299657
Pelle Almquist <per.almquist@gmail.com>
2017-10-22 17:44:22 +0200

    Respect URL query parameters

--- app/src/Search.js
+++ app/src/Search.js
@@ -1,27 +1,39 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+import qs from 'query-string';
 
 import {searchQueryChanged} from './actions'
 import searchButton from './search-button.svg'
 import './Search.css'
 
 function Search(props) {
+  if (props.location.search) {
+    const result = qs.parse(props.location.search)
+    props.onChange(result.searchQuery)
+  }
+
   return (
     <div className="Search">
       <img className="Search-button" alt='search-button' src={searchButton} />
-      <input className="Search-input" type="text" onChange={props.onChange}/>
+      <input
+        className="Search-input"
+        type="text"
+        onChange={(ev) => props.onChange(ev.target.value, true)}/>
     </div>
   )
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch, props) {
   return {
-    onChange: (ev) => {
-      const searchQuery = ev.target.value
-      console.log({searchQuery})
+    onChange: (searchQuery, pushing) => {
       dispatch(searchQueryChanged(searchQuery))
+      const historyPush = props.history.push
+      if (pushing) {
+        historyPush({search: qs.stringify({searchQuery})})
+      }
     }
   }
 }
 
-export default connect(null, mapDispatchToProps)(Search)
+export default withRouter(connect(null, mapDispatchToProps)(Search))
