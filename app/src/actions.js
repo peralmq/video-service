97446436cf503c6c676cb5d59a863dd3e4cad606
Pelle Almquist <per.almquist@gmail.com>
2017-10-25 12:52:48 +0200

    Enable local development backend proxy

--- app/src/actions.js
+++ app/src/actions.js
@@ -1,10 +1,10 @@
-import fetch from 'whatwg-fetch'
+import 'whatwg-fetch'
 export const REQUEST_CREATE_POST = 'REQUEST_CREATE_POST'
 
 export function createPost({title, description, video}) {
   return (dispatch) => {
     dispatch({type: REQUEST_CREATE_POST})
-    fetch('/api/posts', {
+    fetch('/api/posts/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
